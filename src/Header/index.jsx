20fa889fcf31baba04baf5d06016ae9e2edd2cd8
Javier Albornoz <javieraap10@gmail.com>
2022-01-20 19:39:45 -0400

    Se agrego un comentario al final

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -14,3 +14,5 @@ export {Header};
 //Aca se reparte el loading en los hijos del header ( ToDOCounter y ToDoSearch) 
 //con Children para que pueda funcionar el cloneElement 
 //ya que si no se mapea los hijos no puede funcionar con varios hijos
+///////////////////////////////
+//React.Children tiene varias herramientas y la que se implemento ayuda para poder enviarle varios hijos al cloneElement
